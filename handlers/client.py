5adb14f279eb0fdddbc7e03c41a5462731ca280f
Aigerim <tynaibekovaaigerim24@gmail.com>
2023-03-31 14:59:10 +0600

    hw-6

--- handlers/client.py
+++ handlers/client.py
@@ -1,8 +1,9 @@
 from aiogram import types, Dispatcher
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ParseMode
 from config import bot, dp
 from keyboards.client_kb import start_markup
 from database.bot_db import sql_command_random
+from parser.animation import parser
 
 
 @dp.message_handler(commands=['start'])
@@ -57,8 +58,25 @@ async def get_random_user(message: types.Message):
     )
 
 
+async def get_animation(message: types.Message):
+    animation = parser()
+    for anime in animation:
+        await message.answer(
+            # f"{anime['link']}\n\n"
+            f"<b><a href='{anime['link']}'>{anime['title']}</a></b>\n"
+            f"#Y{anime['year']}\n"
+            f"#{anime['genre']}\n"
+            f"#{anime['country']}\n",
+            reply_markup=InlineKeyboardMarkup().add(
+                InlineKeyboardButton("СМОТРЕТЬ", url=anime['link'])
+            ),
+            parse_mode=ParseMode.HTML
+        )
+
+
 def register_handlers_client(dp: Dispatcher):
     dp.register_message_handler(start_command, commands=['start'])
     dp.register_message_handler(help_command, commands=['help'])
     dp.register_message_handler(quiz_1, commands=['quiz'])
     dp.register_message_handler(get_random_user, commands=['get'])
+    dp.register_message_handler(get_animation, commands=['animation'])
---
+++ parser/animation.py
@@ -0,0 +1,53 @@
+from pprint import pprint
+
+import requests
+from bs4 import BeautifulSoup
+
+
+URL = 'https://rezka.ag/animation/'
+
+HEADERS = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'
+}
+
+
+def get_html(url):
+    response = requests.get(url=url, headers=HEADERS)
+    return response
+
+
+def get_data_from_page(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    items = soup.find_all(
+        'div', class_="b-content__inline_item"
+    )
+    animation = []
+    for item in items:
+        info = item.find('div', class_="b-content__inline_item-link").find('div').getText().split(', ')
+        anime = {
+            'title': item.find('div', class_="b-content__inline_item-link").find('a').getText(),
+            'link': item.find('div', class_="b-content__inline_item-link").find('a').get('href'),
+            'year': info[0],
+            'country': info[1],
+            'genre': info[2]
+        }
+        animation.append(anime)
+    return animation
+
+
+def parser():
+    html = get_html(URL)
+    if html.status_code == 200:
+        animation = []
+        for i in range(1, 2):
+            html = get_html(f"{URL}page/{i}/")
+            current_page = get_data_from_page(html.text)
+            animation.extend(current_page)
+        return animation
+    else:
+        raise Exception("Error in parser")
+
+
+
+
